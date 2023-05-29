6db03c1dda87cab3dbfee1721333ed02eb82f880
miya0619 <his.miyam.0619@gmail.com>
2023-05-29 23:08:46 +0900

    ログインフォーム作成、バリデーション設定

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[AuthController::class,'showLogin'])->name('showLogin');
+Route::post('login',[AuthController::class,'login'])->name('login');
